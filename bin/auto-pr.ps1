3a3eed293c03f3c9741e198f77480da78bf4d0af
Shijie Liu <prclsj@outlook.com>
2024-09-17 07:07:22 +0800

    add goldendict-ng

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "1nkwell/inkwell:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
